6d42c3ef438c469ea3ad326f8a2240cf16994f3d
Rafa <daniniviru@gmail.com>
2020-05-01 12:20:29 +0200

    descomposición

---
+++ practica/descomposicionhoraria.py
@@ -0,0 +1,8 @@
+while True:
+    tiempo = int(input("Número positivo de segundos: "))
+    if tiempo > 0:
+        print("Horas: " + str(tiempo // 3600))
+        minutos = int((tiempo % 3600) / 60)
+        segundos = int((tiempo % 3600) % 60)
+        print("Minutos: " + str(minutos))
+        print("Segundos: " + str(segundos))
